676a42cde1712a1a3c4929473e7bd9291a836155
Pavel Zaytsev <pavel@iberisoft.com>
2026-03-30 08:53:27 +0900

    자바스크립트_라이선스불필요

--- PeltierControlApp/Program.cs
+++ PeltierControlApp/Program.cs
@@ -1,7 +1,4 @@
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
+
 using PeltierControlApp.Components;
 using PeltierControlApp.Services;
 
@@ -10,11 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // .NET 8 대화형 서버 서비스 등록 (매우 중요)
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-// Blazorise 및 펠티어 서비스 등록
-builder.Services.AddBlazorise(options => { options.Immediate = true; })
-    .AddBootstrapProviders()
-    .AddFontAwesomeIcons();
 
 builder.Services.AddSingleton<PeltierService>();
 
